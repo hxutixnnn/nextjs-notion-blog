9e51721f6754ce5364251d4f82f2b0294ea4b67f
Nguyen Huu Tien <43488931+hxutixnnn@users.noreply.github.com>
2021-08-11 10:45:16 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'a985ee88c1d94ff28107cf36402af3bb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'My Personal Blog',
+  domain: 'nguyenhuutien.com',
+  author: 'Huu-Tien Nguyen',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'I am a Front-End Developer based in Vietnam 🇻🇳',
+  socialImageTitle: 'My Personal Blog',
+  socialImageSubtitle: "Hey 👋, I'm Huu-Tien Nguyen",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'hxutixnnn',
+  github: 'hxutixnnn',
+  linkedin: 'hxutixnnn',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageIcon: '👨🏻‍💻',
+  defaultPageCover: 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=3600',
   defaultPageCoverPosition: 0.5,
 
   // image CDN host to proxy all image requests through (optional)
